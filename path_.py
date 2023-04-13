648595d37a936b01eeb29dc74631b14c0493a33a
user <1253417392@qq.com>
2023-04-13 18:05:33 +0800

    去除pint

--- path_.py
+++ path_.py
@@ -7,6 +7,5 @@ ini_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config", "
 switch_ini = (os.path.join(os.path.dirname(os.path.realpath(__file__)), "config", "switch.ini"))
 yam_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "yaml_data", "data.yaml")
 middleground = os.path.join(os.path.dirname(os.path.realpath(__file__)), "yaml_data", "middleground.yaml")
-print(middleground)
 # print(switch_ini)
 # print(yam_path)
